90de331aa78f4515dfa262f113a3c1d43fc39525
Emerson Millard <emillard.code@gmail.com>
2025-03-28 12:03:00 -0700

    Added challenge solution.

--- src/main/java/com/project/solution/LeetCodeSolution.java
+++ src/main/java/com/project/solution/LeetCodeSolution.java
@@ -4,6 +4,33 @@ public class LeetCodeSolution {
 
     public static void main(String[] args) {
 
+        int[] nums1 = { -2, -1, -1, 1, 2, 3 };
+        System.out.println(maximumCount(nums1));
+
+        int[] nums2 = { -3, -2, -1, 0, 0, 1, 2 };
+        System.out.println(maximumCount(nums2));
+
+        int[] nums3 = { 5, 20, 66, 1314 };
+        System.out.println(maximumCount(nums3));
+
+    }
+
+    public static int maximumCount(int[] nums) {
+
+        int positiveCount = 0, negativeCount = 0;
+
+        for (int num : nums) {
+
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            }
+
+        }
+
+        return Math.max(positiveCount, negativeCount);
+
     }
 
 }
